964f3cf1928e4284ee49d76e0b096a09217081e4
gusdydgud <gusdyddla@gmail.com>
2024-06-15 21:34:45 +0900

    [level 0] Title: A로 B 만들기, Time: 0.52 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 0;
+        char b[] = before.toCharArray();
+        char a[] = after.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(Arrays.equals(a,b)){
+            answer =1;
+        }
+        return answer;
+    }
+}
