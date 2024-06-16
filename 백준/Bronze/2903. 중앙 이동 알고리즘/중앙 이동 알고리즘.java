82f2003f74a07ef1282151b5842cba2a1cc0d207
gusdydgud <gusdyddla@gmail.com>
2024-06-16 13:18:43 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a=2;
+        int re=0;
+        while(t>0){
+            re= 2*a-1;
+            a=2*a-1;
+            t--;
+        }
+        System.out.println(re*re);
+        
+        
+
+        
+
+        
+       
+
+     
+       
+    }
+    
+  
+}
