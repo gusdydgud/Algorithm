f84604ee2455b04e3056f4d673b4d0b28e46ed07
gusdydgud <gusdyddla@gmail.com>
2024-06-28 09:00:50 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,27 +1,22 @@
-class Solution {
-	public int solution(int a, int b) {
-    int gcd = gcd(a, b);
-        
-        // 기약분수의 분모 계산
-        int denominator = b / gcd;
-        
-        // 분모가 2와 5로만 나누어지는지 확인
-        while (denominator % 2 == 0) {
-            denominator /= 2;
-        }
-        while (denominator % 5 == 0) {
-            denominator /= 5;
-        }
-        
-        // 최종적으로 분모가 1이 되면 유한소수, 그렇지 않으면 무한소수
-        return denominator == 1 ? 1 : 2;
-    }
+public class Solution {
+    	
+    public int solution(int a, int b) {
+    	
+      if(b == 1) return 1;
+    	
+    	int gcd = 0;
+    	
+    	for(int i = Math.min(a, b); i > 0; i--) {
+    		if(a % i == 0 && b % i == 0) {
+    			gcd = i; break;
+    		}
+    	}
+    	
+    	b /= gcd ;
+    	
+    	while (b % 2 == 0) b /= 2;
+    	while (b % 5 == 0) b /= 5;
 
-    // 유클리드 호제법을 이용한 최대공약수 계산 함수
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
+        return (b == 1) ? 1 : 2;       
     }
 }
