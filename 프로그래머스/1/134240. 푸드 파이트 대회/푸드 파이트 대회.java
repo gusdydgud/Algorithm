2162fa49df347e9dde3ba115da673eed3920e27b
gusdydgud <gusdyddla@gmail.com>
2024-08-18 11:48:07 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 29.40 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i=1 ;i<food.length;i++){
+            for(int j=0;j<food[i]/2;j++){
+                answer += Integer.toString(i);
+            }
+        }
+        String answer2 ="";
+        for(int i=answer.length()-1;i>=0;i--){
+            answer2+=answer.charAt(i);
+        }
+        return answer+"0"+answer2;
+    }
+}
