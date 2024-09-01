3c2270cf6847c04cfe413d8209953d3e7003b6e9
gusdydgud <gusdyddla@gmail.com>
2024-09-01 21:58:47 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 20620 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String command = br.readLine();
+
+            if (command.startsWith("push")) {
+                int value = Integer.parseInt(command.split(" ")[1]);
+                stack.push(value);
+            } else if (command.equals("pop")) {
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.pop()).append("\n");
+                }
+            } else if (command.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if (command.equals("empty")) {
+                if (stack.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (command.equals("top")) {
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.peek()).append("\n");
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
